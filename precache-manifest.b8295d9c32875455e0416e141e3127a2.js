e478e7ac3628cd5aebd3b89b3ff8485103152b28
Jose <jose@drift.codes>
2019-05-04 21:01:52 +0200

    Updates

---
+++ precache-manifest.b8295d9c32875455e0416e141e3127a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "5cc8e6662400de56b1d6",
+    "url": "/contactmanager/static/js/main.fb40546c.chunk.js"
+  },
+  {
+    "revision": "8c3d32dda7d3081f5b51",
+    "url": "/contactmanager/static/js/2.fe763a5c.chunk.js"
+  },
+  {
+    "revision": "5cc8e6662400de56b1d6",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "8c3d32dda7d3081f5b51",
+    "url": "/contactmanager/static/css/2.0e3ece82.chunk.css"
+  },
+  {
+    "revision": "b415d3bc60ff1aaa5e12050622935de5",
+    "url": "/contactmanager/index.html"
+  }
+];
