8943a3743b4f821664dff59a14a12ef1ab065b11
Jose <jose@drift.codes>
2019-05-04 20:54:44 +0200

    Initial commit

---
+++ src/components/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function about() {
+  return (
+    <div>
+      <h1 className="display-4">About Contact Manager</h1>
+      <p className="lead">Simple app to manage</p>
+      <p className="text-secondary">sjkzv</p>
+    </div>
+  );
+}
---
+++ src/components/test/Test.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+class Test extends Component {
+  state = {
+    title: "",
+    body: ""
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          title: data.title,
+          body: data.body
+        })
+      );
+  }
+
+  componentWillMount() {
+    console.log("will mount");
+  }
+
+  componentDidUpdate() {
+    console.log("did update");
+  }
+
+  componentWillUpdate() {
+    console.log("will update");
+  }
+
+  componentWillReceiveProps() {
+    console.log("will receive props");
+  }
+
+  render() {
+    const { title, body } = this.state;
+    return (
+      <div>
+        <h1>{title}</h1>
+        <p>{body}</p>
+      </div>
+    );
+  }
+}
+
+export default Test;
